0ab01bd4da6f016939b8de95a46b4721c470cfbd
kdkdhoho <hkim4410@naver.com>
2023-03-01 15:59:58 +0900

    feat(SimpleList): 정적 팩터리 메서드 생성

--- src/main/java/techcourse/jcf/mission/SimpleList.java
+++ src/main/java/techcourse/jcf/mission/SimpleList.java
@@ -3,6 +3,8 @@ package techcourse.jcf.mission;
 // TODO: 제네릭을 사용하지 않는다. String만 다루는 리스트를 구현한다.
 public interface SimpleList {
 
+    String[] EMPTY_ARRAY = {};
+
     boolean add(String value);
 
     void add(int index, String value);
@@ -24,4 +26,39 @@ public interface SimpleList {
     String remove(int index);
 
     void clear();
+
+    static String[] of() {
+        return EMPTY_ARRAY;
+    }
+
+    static String[] of(String s1) {
+        return new String[]{s1};
+    }
+
+    static String[] of(String s1, String s2) {
+        return new String[]{s1, s2};
+    }
+
+    static String[] of(String s1, String s2, String s3) {
+        return new String[]{s1, s2, s3};
+    }
+
+    static String[] of(String s1, String s2, String s3, String s4) {
+        return new String[]{s1, s2, s3, s4};
+    }
+
+    static String[] of(String s1, String s2, String s3, String s4, String s5) {
+        return new String[]{s1, s2, s3, s4, s5};
+    }
+
+    static String[] of(String s1, String s2, String s3, String s4, String s5, String s6) {
+        return new String[]{s1, s2, s3, s4, s5, s6};
+    }
+
+    static String[] of(String... strings) {
+        if (strings.length == 0) {
+            return EMPTY_ARRAY;
+        }
+        return strings;
+    }
 }
